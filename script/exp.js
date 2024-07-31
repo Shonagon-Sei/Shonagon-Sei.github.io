fd4d53f1561a8bfc44319c68f1db9e921b29c6c5
Iyako <pannawish.boo@mail.kmutt.ac.th>
2024-07-31 21:41:13 +0700

    Image Zoom Fix?

--- script/exp.js
+++ script/exp.js
@@ -4,7 +4,7 @@ function div_img() {
   var no = document.getElementById('nubo');
   var s = document.getElementById('eb');
   var type = document.getElementById('cts');
-
+  var dpim = document.getElementById('dpim')
   var illust = document.getElementById('illust');
   var set = document.getElementById('set');
   var flavText = document.getElementById('flavortext')
@@ -18,6 +18,9 @@ function div_img() {
   var eb2 = document.getElementById('eb2');
   var nName = document.getElementById('nname')
 var nName1 = document.getElementById('nname1')
+
+  var scrollT = dpim.scrollTop
+  var scrollL = dpim.scrollLeft
 
   illust.style.transform = "translate(-12px, -9.2px)";
   set.style.transform = "translate(25px, -9.2px)";
@@ -34,8 +37,7 @@ var nName1 = document.getElementById('nname1')
 
 
 
-  d_.style.border = '0';
-  no.style.transform = "translate(0 , -1.2px)";
+  
   //s.style.transform = "translate(0 , -1.2px)";
 
   var svgElements = document.body.querySelectorAll('svg');
@@ -81,9 +83,49 @@ function div_img1() {
   var n_ = document.getElementById('nub');
   var no = document.getElementById('nubo');
   var s = document.getElementById('eb');
+  var type = document.getElementById('cts');
+  var dpim = document.getElementById('dpim')
+  var illust = document.getElementById('illust');
+  var set = document.getElementById('set');
+  var flavText = document.getElementById('flavortext')
+  var se = document.getElementById('se');
+  var pw = document.getElementById('pw');
+  var pw1 = document.getElementById('pw1');
+  var cn = document.getElementById('cns');
+  var rec = document.getElementById('rec');
+  var rec1 = document.getElementById('rec1');
+  var eb = document.getElementById('eb');
+  var eb2 = document.getElementById('eb2');
+  var nName = document.getElementById('nname')
+var nName1 = document.getElementById('nname1')
+
+  var scrollT = dpim.scrollTop
+  var scrollL = dpim.scrollLeft
+
+  illust.style.transform = "translate(-12px, -9.2px)";
+  set.style.transform = "translate(25px, -9.2px)";
+  
+  se.style.transform = `translate(0px, ${((type.value === 'ma') ? '20px':'0px')})`
+  pw.style.translate = "0px -1.2px";
+  pw1.style.translate = "0px -1.2px";
+  cn.style.translate = `0px -2px`
+  rec.style.transform = `translate(${((type.value === 'tk') ? '257px, -21px' :  '242.75px, -22.2px')})`;
+  rec1.style.transform = `translate(${((type.value === 'tk') ? '257px, -21px' :  '242.75px, -22.2px')})`;
+  nName.style.transform = `translate(242.75px, -37.2px)`
+  nName1.style.transform = `translate(242.75px, -37.2px)`
+  eb.style.transform = `translate(0px, ${((type.value === 'ma') ? '18.8px':'-1.2px')})`
+  //
 
   d_.style.border = '0';
-  //
+  no.style.transform = "translate(0 , -1.2px)";
+  let scroll_obj = {
+    behavior: 'smooth',
+    top: scrollL,
+    left: scrollT
+  };
+
+  
+  dpim.scroll(scroll_obj)
   var fileName = prompt('Please Enter File Name')
 
   html2canvas(d_, { scale: 6.665 ,backgroundColor: null}).then(function(canvas) {
@@ -96,8 +138,18 @@ function div_img1() {
     link.click();
 
     d_.style.border = '';
-    n_.style.transform = 'translate(0, 0.5px)';
-    no.style.transform = 'translate(0, 0.5px)';
+    n_.style.transform = '';
+    no.style.transform = '';
+    s.style.transform = '';
+    se.style.transform = ''
+    pw.style.translate = ''
+    pw1.style.translate = ''
+    cn.style.translate = ''
+    rec.style.transform = ''
+    rec1.style.transform = ''
+    nname.style.transform = ''
+    nname1.style.transform = ''
+    eb.style.transform = ''
     
   });
 }
