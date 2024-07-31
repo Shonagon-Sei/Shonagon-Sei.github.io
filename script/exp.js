be9a1f7ec72b4d658221be36913659b34f07624f
Iyako <pannawish.boo@mail.kmutt.ac.th>
2024-07-31 21:56:17 +0700

    Scroll Fix Try 2

--- script/exp.js
+++ script/exp.js
@@ -50,7 +50,7 @@ var nName1 = document.getElementById('nname1')
 
   //
   var fileName = prompt('Please Enter File Name')
-  html2canvas(d_, { scale: 6.665 ,backgroundColor: null, allowTaint:true}).then(function(canvas) {
+  html2canvas(d_, { scale: 6.665 ,backgroundColor: null, allowTaint:true, scrollX: scrollL, scrollY:scrollT}).then(function(canvas) {
     var image = new Image();
     image.src = canvas.toDataURL();
 
